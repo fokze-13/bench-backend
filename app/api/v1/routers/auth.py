b0479cc1485f02ebba823d9ae7220654ba7c4333
fokze-13 <108113475+fokze-13@users.noreply.github.com>
2026-05-23 15:40:44 +0500

    user register logic

--- app/api/v1/routers/auth.py
+++ app/api/v1/routers/auth.py
@@ -1,10 +1,21 @@
+from typing import Annotated
 from fastapi import APIRouter
+from app.exceptions import UnexpectedError
 from app.schemas.auth import TokenRead, TokenCreate
-
+from app.api.v1.deps.user_deps import get_user_service
+from fastapi import Depends
+from app.services.user_service import UserService
 
 router = APIRouter(prefix="/auth")
+UserServiceDep = Annotated[UserService, Depends(get_user_service)]
 
 
 @router.post("/get_token", response_model=TokenRead)
-async def get_token(body: TokenCreate):
-    pass
+async def get_token(body: TokenCreate, user_service: UserServiceDep):
+    try:
+        device_id = body.device_id
+        await user_service.register(device_id)
+
+        # TODO: token logic
+    except Exception:
+        raise UnexpectedError
